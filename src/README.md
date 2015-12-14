1c3f86a269c8a6cb7f157d64fca685806b72dc30
CassMelo <cassiac@yahoo.com>
2015-12-14 14:07:50 -0600

    Project 5 done

---
+++ src/README.md
@@ -0,0 +1,52 @@
+#Udacity FrondEnd Developer Class
+Version 1.0 12/14/2015
+
+
+##General Usage Notes
+This project is part of the Udacity FrontEnd Developer Course. In this project we had to work with API and Google Maps.
+
+## Installing
+To install just copy all files under the directory dist and run the file index.html
+
+##How to run the application
+
+
+Once the webpage is open is going to show a map of an areaclose to my neighborhood and 20 locations I'm interested in.
+Also, it'll show a list of these locations on the left. If the screen is less than 850px the location list will be hidden and
+you need to press the hamburger icon (on the left top corner) to open the location list. The location list will disappear after
+a search or when you click anywhere in the map area.
+
+###Searching a location
+If you would like to narrow the location list you can type a string on the input that says "Filter Location" an press the **Filter** button.
+The list will updated will all locations that contain the string typed. The map will show only the markers related to those locations.
+
+I you want to see all locations again, just have blank on the search input and press the **Filter** button.
+
+
+###Selecting a location
+You can select a location either clicking on the map marker or at the location name.
+When a location is selected, the map marker will bounce and a window with the location name and phone number will appear.
+In case the phone number wasn't available at foursquare website, just the name will appear.
+If a location is selected before a search is going to be performed, the selection will disappear after the search is done.
+
+
+##Author
+Cassia De Melo
+
+email: cassiac@yahoo.com
+
+##Copyright (C) 2015 Cassia
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see [License](http://www.gnu.org/licenses/).
+
---
+++ src/js/app.js
@@ -0,0 +1,239 @@
+var ViewModel = function(){
+
+	var self = this; // in this case, self is the View_Model
+	var mapObj;
+	self.filterLocStr = ko.observable();
+	self.selectedLoc =  ko.observable('');
+
+
+	/* Initialize Neighborhood Information */
+	self.street = ko.observable('1000 S La Grange Road');
+	self.city = ko.observable('Orland Park');
+	self.locations = ko.observableArray();
+	self.map = ko.observable({
+		lat: ko.observable(41.602631),
+		lng: ko.observable(-87.853004)
+	});
+	self.address = ko.computed(function() {
+		return self.street() + ', ' + self.city();
+	},this);
+
+
+	/* Initialize Map from Google Maps*/
+	ko.bindingHandlers.map = {
+
+	    init: function (element, valueAccessor, allBindingsAccessor, viewModel) {
+
+	        var mapObj = ko.utils.unwrapObservable(valueAccessor());
+
+	        var latLng = new google.maps.LatLng(
+	            ko.utils.unwrapObservable(mapObj.lat),
+	            ko.utils.unwrapObservable(mapObj.lng));
+
+	        var mapOptions = { center: latLng,
+	                          zoom: 12,
+	                          mapTypeId: google.maps.MapTypeId.ROADMAP};
+
+	        mapObj.googleMap = new google.maps.Map(element, mapOptions);
+
+	        self.mapObj = mapObj.googleMap;
+
+	        // Return locations according to the locInit
+	        var locInit = ['park','restaurant'];
+
+	        self.getLocations(mapObj.googleMap, latLng, locInit);
+    	} // init
+	}; // bindingHandlers.map
+
+
+
+	/*************************** get locations ***/
+	this.getLocations = function(map, latLng, searchStr){
+
+		// Specify location, radius and place types for our Places API search.
+	  	var request = {
+	    	location: latLng,
+	    	radius: '5000',
+	    	types: searchStr
+	  	};
+
+		// Create the PlaceService and send the request.
+	    // Handle the callback with an anonymous function.
+	  	var service = new google.maps.places.PlacesService(map);
+	  	var place;
+	  	var marker;
+	  	service.nearbySearch(request, function(results, status) {
+	  		if (status == google.maps.places.PlacesServiceStatus.OK) {
+
+				// Load Array locations with the result of the Search
+		      	var location = ko.utils.arrayMap(results, function(place) {
+
+
+		  			return {name: ko.observable(place.name),
+				 			address: ko.observable(place.vicinity),
+				 			position: place.geometry.location,
+				 			marker: ko.observable(new google.maps.Marker({
+			          									map: map,
+			          									position: place.geometry.location,
+			          									title: place.name ,
+			          									vicinity:  place.vicinity,
+			          									animation: null
+			        								})),
+				 			infoWindow: ko.observable(new google.maps.InfoWindow({
+															content: place.name
+															})),
+				 			infoWindowStr: ko.observable(place.name),
+				 			visible: ko.observable(true),
+				 			selected: ko.observable(false)};
+		 			 		});
+
+
+		  		self.locations(location);
+				// add event listener for each marker
+				for (var i = 0; i<self.locations().length; i++){
+						var marker = self.locations()[i].marker();
+
+						self.getAdditionalInfo(self.locations()[i]);
+
+						marker.addListener('click', (function(markerCopy){
+							return function(){
+								self.selectLoc('2',markerCopy);
+							};
+						})(marker));
+				}// for
+	    	} //if
+		}); // nearbySearch
+	}; // getLocations
+
+	this.getAdditionalInfo = function(loc){
+
+			// get additional info to put on the infowindow.
+			var phoneStr = "";
+
+
+		    var foursquareURL = 'https://api.foursquare.com/v2/venues/search?ll='+loc.position.lat()+','+ loc.position.lng()+ '&client_id=DTKUKGC3TBNDP52LDDPYIELISP5FVIVXY4G1RGS5TIM1IXNS&client_secret=IOXARMZ4DO4YMGG3XMDUH3UB5RRSBBFJEPKBUZYF14EU3TIO&v=20151213';
+		    $.getJSON(foursquareURL, function(data) {
+
+		        venues = data.response.venues;
+
+		        if (typeof  venues[0].contact.formattedPhone === 'undefined') {
+		        	phoneStr = '';
+		        } else {
+		        	phoneStr = '<br/>Phone: ' + venues[0].contact.formattedPhone;
+		        }
+
+				loc.infoWindowStr(loc.infoWindowStr() +  phoneStr);
+
+		    }) .fail(function( ) {
+		            console.log( "Request Failed: " + err );
+		        });
+
+	};// getAdditionalInfo
+
+
+	this.filterLoc = function(clickedBtn) {
+
+		// filter locations based on the searchStrinf typed by the user
+
+		for (var i = 0; i<self.locations().length; i++){
+			if (self.locations()[i].name().toUpperCase().search(self.filterLocStr().toUpperCase()) < 0){
+				self.locations()[i].visible(false);
+				self.locations()[i].marker().setMap(null);
+			} else{
+				self.locations()[i].visible(true);
+				self.locations()[i].marker().setMap(self.mapObj);
+			}// if
+			self.locations()[i].selected(false);
+			self.infoWindow(self.locations()[i],'close');
+		}// for
+		self.selectedLoc("");
+	}; // filterLoc
+
+
+	this.selectLoc = function(Origin,clickedItem){
+
+		var loc;
+
+
+	    switch (Origin) {
+		    case '1': // clicked the loc list
+	    		loc = clickedItem;
+		        break;
+		    case '2': // clicked the marker
+	    		loc = self.getLoc(clickedItem);
+		        break;
+		}// switch
+
+		// Highlight loc list
+		self.highlightLoc(loc);
+
+		// Bounce map marker
+		self.toggleBounce(loc.marker());
+
+		// Open InfoWindow
+		self.infoWindow(loc,'open');
+
+		// Checks if the selected location is different from the previous selected.
+		// If so unmark the previous one
+
+		if (self.selectedLoc() === "" ){
+			self.selectedLoc(loc);
+		} else {
+			if (self.selectedLoc() === loc) {
+				self.selectedLoc("");
+			} else {
+				self.highlightLoc(self.selectedLoc());
+				self.toggleBounce(self.selectedLoc().marker());
+				self.infoWindow(self.selectedLoc(),'close');
+				self.selectedLoc(loc);
+			}
+		}
+	};// selectLoc
+
+	this.infoWindow = function(loc, action) {
+
+		loc.infoWindow().setContent(loc.infoWindowStr());
+
+		if (action == 'open'){
+			loc.infoWindow().open(self.mapObj, loc.marker());
+		}  	else {
+			loc.infoWindow().close(self.mapObj, loc.marker());
+		}
+	}; // infoWindow
+
+
+	// returns the location that has a specific marker
+	this.getLoc = function(marker){
+		var i;
+		for (i = 0; i<self.locations().length; i++){
+			if (self.locations()[i].marker() == marker){
+				return self.locations()[i];
+			}// if
+		}// for
+		return null;
+	};// getLoc
+
+
+    this.toggleBounce = function(marker) {
+		if (marker.getAnimation() !== null) {
+			marker.setAnimation(null);
+		} else {
+		    marker.setAnimation(google.maps.Animation.BOUNCE);
+		}
+	};// toggleBounce
+
+	this.highlightLoc= function(loc) {
+
+		if (loc.selected()) {
+			loc.selected(false);
+		} else {
+			loc.selected(true);
+		}
+	};// highlightLoc
+
+
+}; // ViewModel
+
+
+
+ko.applyBindings(new ViewModel());
